9da8fd58931edca1da1da96f018e86aeb5716cf4
Carlos Grossi <carlos.grossi@gmail.com>
2016-03-14 19:40:35 -0300

    Initial Commit

---
+++ IAPKit/IAPKit.h
@@ -0,0 +1,19 @@
+//
+//  IAPKit.h
+//  IAPKit
+//
+//  Created by Carlos Grossi on 14/3/16.
+//  Copyright © 2016 Carlos Grossi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IAPKit.
+FOUNDATION_EXPORT double IAPKitVersionNumber;
+
+//! Project version string for IAPKit.
+FOUNDATION_EXPORT const unsigned char IAPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IAPKit/PublicHeader.h>
+
+
